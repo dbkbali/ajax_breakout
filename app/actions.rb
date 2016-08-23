4a8eea7f4fc38ffc13eedd9ff57e1cc8d9f059a5
dbkbali <david@what2do.asia>
2016-08-23 17:28:55 -0400

    simple app to demo jQuery Ajax

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,37 @@
 # Homepage (Root path)
+
+CARDS = [
+  {
+    name: "Pikachu",
+    skill: "Thunderbolt",
+    image: "http://static.giantbomb.com/uploads/scale_small/0/6087/2437349-pikachu.png",
+    hp: 25,
+    type: "Electric"
+  },
+  {
+    name: "Magikarp",
+    skill: "Splash",
+    image: "http://static.giantbomb.com/uploads/scale_small/13/135472/1898291-129magikarp.png",
+    hp: 10,
+    type: "Water"
+  },
+  {
+    name: "Jigglypuff",
+    skill: "Lullaby",
+    image: "http://static.giantbomb.com/uploads/scale_small/13/135472/1892299-039jigglypuff.png",
+    hp: 15,
+    type: "Fairy"
+  }
+]
+
 get '/' do
   erb :index
+end
+
+get '/cards' do
+  CARDS.to_json
+end
+
+post '/cards' do 
+  params.to_json  
 end
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,27 @@
 $(document).ready(function() {
+  // alert('hello world');
+
+  var appendCards = function (data) {
+    console.log(data)
+    data.forEach(function(card, i) {
+      $('#results').append(
+        [$('<h3/>').text(card.name),
+        $('<img>').attr('src',card.image)]
+      )
+    })
+  }
+
+  $(document).on("click","#subbutton", function (e) {
+    e.preventDefault();
+    var url = '/cards'
+    var data = {name: $('#name').val()}
+    $.post(url, data, function (response) {
+      console.log(response)
+    })
+
+  } )
+
+  $.getJSON('/cards', appendCards);
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
   // 
@@ -24,29 +47,31 @@ $(document).ready(function() {
         //     "photo_url": "http://www.famousbirthdays.com/thumbnails/hinojosa-stacy-large.jpg"
         // },
   // 
-  var root_url = 'https://daxeel-celebinfo-v1.p.mashape.com/custom/'  // custom/{search_type}
+//   var root_url = 'https://daxeel-celebinfo-v1.p.mashape.com/custom/';  // custom/{search_type}
 
-  // $.getJson
+//   var data = "tommorrow";
 
-  // var data = "tommorrow"
+//    // $.getJSON(root_url + data,{}, function (response) {
+//    //   console.log(response);
+//    // })
+//   // 
+//   // $.ajax 
+  
+//   var cbAppend = function (response) {
+//     response.data.forEach(function(celeb, i) {
+//       $('#results').append(
+//         $('<div/>').append($('<h3/>').text(JSON.stringify(celeb))));
+//     });
+//   }
 
-  // $.getJSON(root_url, data, function (response) {
-  //   console.log(response)
-  // })
-  // 
-  // $.ajax 
-  // $.ajax({
-  //     beforeSend: function(xhr) {
-  //        xhr.setRequestHeader("X-Mashape-Key", "eeh0PJsgSwmshRhoXCukn2DX8veUp1eyGcejsnX6KKF5R6gKOy"); // Enter here your Mashape key
-  //        },
-  //     url: root_url + 'today',
-  //     method: 'GET',
-  //     dataType: 'json',
-  //     success: function(response) { 
-  //             console.log(response);
-  //         },
-  //     error: function() { alert('boo!'); }
-       
-  //   });
+//   $.ajax({
+//       beforeSend: function(xhr) {
+//          xhr.setRequestHeader("X-Mashape-Key", "eeh0PJsgSwmshRhoXCukn2DX8veUp1eyGcejsnX6KKF5R6gKOy"); // Enter here your Mashape key
+//          },
+//       url: root_url + 'today',
+//       method: 'GET',
+//       dataType: 'json'
+//     }).then(cbAppend);
 
+// 
 });
